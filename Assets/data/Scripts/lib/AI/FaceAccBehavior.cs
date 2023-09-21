29beb924dc2754f30ad5bfe76ad14dd52d9595d5
Redfrog80 <90211112+Redfrog80@users.noreply.github.com>
2023-09-21 00:27:09 -0500

    Added behaviors which enable rotation

---
+++ Assets/data/Scripts/lib/AI/FaceAccBehavior.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceAccBehavior : Steering
+{    // Start is called before the first frame update
+    public override SteeringData GetSteering(SteeringBehaviorBase
+    steeringbase)
+    {
+        Rigidbody2D rb2D =  GetComponent<Rigidbody2D>();
+        Engine engine = GetComponent<Engine>();
+
+        SteeringData steering = new SteeringData();
+
+        Vector2 accel = engine.impulse_vector;
+        float angle =90 - Mathf.Rad2Deg*Mathf.Atan2(accel.y, accel.x) + rb2D.rotation;
+
+        if (Mathf.Abs(angle) > 180)
+            angle-=Mathf.Sign(angle)*360;
+        
+        steering.angular = Mathf.Sign(angle)*engine.maxAngVel;
+        return steering;
+    }
+    
+}
---
+++ Assets/data/Scripts/lib/AI/FaceBehavior.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceBehavior : Steering
+{
+    [SerializeField] private GameObject target;
+    // Start is called before the first frame update
+    public override SteeringData GetSteering(SteeringBehaviorBase
+    steeringbase)
+    {
+        Rigidbody2D rb2D_self =  GetComponent<Rigidbody2D>();
+
+        Engine engine = GetComponent<Engine>();
+
+        SteeringData steering = new SteeringData();
+
+        Vector2 delta = target.transform.position - transform.position;
+        float angle =90 - Mathf.Rad2Deg*Mathf.Atan2(delta.y, delta.x) + rb2D_self.rotation;
+
+        if (Mathf.Abs(angle) > 180)
+            angle-=Mathf.Sign(angle)*360;
+        
+        steering.angular = Mathf.Sign(angle)*engine.maxAngVel;
+        return steering;
+    }
+}
