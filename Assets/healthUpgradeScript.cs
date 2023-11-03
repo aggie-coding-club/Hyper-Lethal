ec942dd0db6d4a6477bbe73d1277b43eb3d007cb
GageHoweGK <howe.gaged@gmail.com>
2023-11-03 17:47:33 -0500

    Create healthUpgradeScript.cs

---
+++ Assets/healthUpgradeScript.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class healthUpgradeScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collision detected");
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Collision with player detected");
+
+            Hull playerHull = collision.gameObject.GetComponent<Hull>();
+            if (playerHull != null)
+            {
+                if (gameObject.name == "healthUpgrade")
+                {
+                    // Increase the player's hull by 10
+                    playerHull.IncreaseHull(10);
+                    float updatedHull = playerHull.getHull();
+                    Debug.Log("Player's hull is now: " + updatedHull);
+
+                    // Destroy the item
+                    Destroy(gameObject);
+
+                    // Log that the item was destroyed
+                    UnityEngine.Debug.Log("Item was destroyed");
+                }
+            }
+        }
+    }
+}
+
